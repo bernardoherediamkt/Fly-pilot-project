4ce8daf8de40624638ffbc8e64a5024255aee74b
bernardoherediamkt <bernardoheredia.mkt@gmail.com>
2026-08-14 23:47:05 -0300

    feat: route /live to real flight search

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import { App } from './App';
+import { LiveSearchPage } from './LiveSearchPage';
+
+const isLiveSearch = window.location.pathname === '/live';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {isLiveSearch ? <LiveSearchPage /> : <App />}
   </React.StrictMode>
 );
